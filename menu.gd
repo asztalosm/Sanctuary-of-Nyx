dd4c11f8a077bbc04e4f7c2060d1d2638a9edc17
asztalosm <23d_asztalosm@nyirszikszi.hu>
2025-08-23 16:18:32 +0200

    Menu mostly complete, created some textures for it, shaders, class selection

--- menu.gd
+++ menu.gd
@@ -1,20 +1,41 @@
 extends Control
-
+var onclassesscreen = false
 func _ready() -> void:
-	$Control/Button.grab_focus()
-
+	$Buttons/Button.grab_focus()
+	await get_tree().create_timer(1).timeout
+	$MenuMusic.play()
+	
 func showclasses():
-	print("should show classes")
+	for children in $Classes/Buttons.get_children():
+		var classtween = get_tree().create_tween()
+		classtween.tween_property(children, "modulate:a", 1, 0.25)
+		await classtween.finished
+		release_focus()
+		$Classes/Button.grab_focus()
+		
 
 
 func _on_button_pressed() -> void:
-	#todo: make this be saved locally, idc that the browser probably won't hold the save
-	var buttontween = get_tree().create_tween()
-	buttontween.set_parallel(true)
-	buttontween.tween_property($Control, "modulate:a", 0, 1.0)
-	buttontween.tween_property($RichTextLabel, "modulate:a", 0, 1.0)
-	buttontween.play()
-	await get_tree().create_timer(1.0).timeout
-	$Camera2D.position.x += 400
-	await get_tree().create_timer(1.0).timeout
-	showclasses()
+	if onclassesscreen == true:
+		return
+	else:
+		$Buttons/Button/AudioStreamPlayer2D.play()
+		onclassesscreen = true
+		#todo: make this be saved locally, idc that the browser probably won't hold the save
+		var buttontween = get_tree().create_tween()
+		buttontween.set_parallel(true)
+		buttontween.tween_property($Buttons, "modulate:a", 0, 1.0)
+		buttontween.tween_property($RichTextLabel, "modulate:a", 0, 1.0)
+		buttontween.play()
+		await get_tree().create_timer(1.0).timeout
+		$Camera2D.position.x += 800
+		await get_tree().create_timer(1.0).timeout
+		showclasses()
+
+
+func _playgame() -> void:
+	get_tree().change_scene_to_file("testplace.tscn")
+
+
+func _on_button_3_pressed() -> void:
+	get_tree().quit()
