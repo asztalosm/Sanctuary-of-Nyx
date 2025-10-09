e2bccd60bb5ad79fdc0f345dc566ae634784813d
Marci <23d_asztalosm@nyirszikszi.hu>
2025-10-09 15:12:04 +0200

    Explosion animation and some hitbox fixes

--- explosive_barrel.gd
+++ explosive_barrel.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 
 func _on_body_entered(body: Node2D) -> void:
-	if body.name == "MageProjectile":
+	if body.name == "MageProjectile" and body.moving:
 		$AnimatedSprite2D.play("default")
 
 func _on_animated_sprite_2d_animation_finished() -> void:
--- spikes.gd
+++ spikes.gd
@@ -1,11 +1,8 @@
 extends Area2D
 
-
-
-
-
-func _on_area_entered(area: Area2D) -> void:
-	if area.get_parent().name == "Player":
-		area.get_parent().hit(2.0, false)
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		body.hit(2.0, false)
 	else:
-		area.get_parent().health -= 2.0
+		body.health -= 2.0
+		body.get_node("AnimationPlayer").play("hit")
