c3886f70560f25506a637f0dd726b4a05d826e7e
Marci <23d_asztalosm@nyirszikszi.hu>
2025-10-21 10:56:16 +0200

    arrow trap now shoots correctly

--- sprite_2d.gd
+++ sprite_2d.gd
@@ -1,7 +1,6 @@
 extends Sprite2D
 @export var target = self
 
-
 func _on_timer_timeout() -> void:
 	var arrowscene = preload("res://arrow.tscn")
 	var arrowinstance = arrowscene.instantiate()
@@ -9,4 +8,7 @@ func _on_timer_timeout() -> void:
 	arrowinstance.global_position = global_position
 	add_child(arrowinstance)
 	arrowinstance.rotation = 0
-	arrowinstance.dir = Vector2.from_angle(get_angle_to(global_position - $Marker2D.global_position) - 0.2)
+	var direction = Vector2.from_angle(global_position.angle_to_point($Marker2D.global_position))
+	arrowinstance.dir = direction
+	print(direction)
+	
